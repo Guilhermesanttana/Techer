3042468ddf7f4afc9864639d43cad6b8c0a19423
Guilherme Santana <guilherme.santana@cwb.techer.com.br>
2019-12-17 18:01:28 -0300

    Aulas 3, 4 e 5 LINQ

---
+++ Loja/Program.cs
@@ -0,0 +1,202 @@
+﻿using LojaDominio.Entidade;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loja
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //  -----------------------------------------------------  //
+            // Objetos Complexos - Aula 03
+
+            /*
+            var produto = new Produto();
+            
+            //Exemplo Where (valor > 4)
+            var produtos = produto.Listar().Where(p => p.Valor > 4);
+
+            foreach(Produto item in produtos)
+            {
+                Console.WriteLine(item.Nome);
+            }
+            Console.ReadKey();
+
+            //Single line
+            new Produto().Listar().ForEach(item => Console.WriteLine(item.Nome));
+            Console.ReadKey();
+
+            //Multi operação
+            new Produto().Listar().ForEach(item =>
+            {
+                Console.WriteLine(item.Nome);
+                Console.WriteLine(item.Valor);
+            });
+            Console.ReadKey();
+
+            //Precisamos converter pra lista pq o IEnumerable não suporta o ForEach desse jeito ai
+            new Produto().Listar().Where(p => p.Valor > 4).ToList().ForEach(item =>
+            {
+                Console.WriteLine(item.Nome);
+                Console.WriteLine(item.Valor);
+            });
+            Console.ReadKey();*/
+
+
+
+
+
+
+            //  -----------------------------------------------------  //
+            //Objetos Complexos com Funções - Aula 04
+            /*
+            var produtos = new Produto().Listar();
+
+            var produtos2 = new Produto().Listar();
+
+
+            //Lista os produtos que contem a letra "M"
+            produtos = produtos.Where(p => p.Nome.Contains("M")).ToList();
+            produtos.ForEach(item => Console.WriteLine(JsonConvert.SerializeObject(item)));
+            Console.ReadKey();
+
+
+            //Lista os produtos que (StartWith) Começa com a letra "M" ou (EndsWith) Termina com "o"
+            produtos2 = produtos2.Where(p => p.Nome.StartsWith("M") || p.Nome.EndsWith("a")).ToList();
+            produtos2.ForEach(item => Console.WriteLine(JsonConvert.SerializeObject(item)));
+            Console.ReadKey();
+
+
+            //Lista os nomes dos produtos que (StartWith) Começa com a letra "M" ou (EndsWith) Termina com "o"
+            var nomes = produtos.Where(p => p.Nome.StartsWith("M") || p.Nome.EndsWith("o"))
+                               .Select(p => p.Nome)
+                               .ToList();
+            nomes.ForEach(item => Console.WriteLine(item));
+            Console.ReadKey();
+
+
+            //Lista os nomes dos produtos apenas com as 3 primeiras letras usando o metodo ProdutoSelecionado
+            var nomes2 = produtos.Where(p => p.Nome.StartsWith("M") || p.Nome.EndsWith("o"))
+                               .Select(p => new ProdutoSelecionado { Name = p.Nome.Substring(0,3), Value = p.Valor })
+                               .ToList();
+
+            nomes2.ForEach(item =>
+            {
+                Console.WriteLine(JsonConvert.SerializeObject(item));
+            });
+            Console.ReadKey();
+            
+
+            //Lista os nomes dos produtos e insere "GUI" aparir do index de caracter 2 usando o metodo ProdutoSelecionado
+            var nomes3 = produtos.Where(p => p.Nome.StartsWith("M") || p.Nome.EndsWith("o"))
+                               .Select(p => new ProdutoSelecionado { Name = p.Nome.Insert(2,"GUI"), Value = p.Valor })
+                               .ToList();
+
+            nomes3.ForEach(item =>
+            {
+                Console.WriteLine(JsonConvert.SerializeObject(item));
+            });
+            Console.ReadKey();
+
+
+            //Ainda existem os comandos            
+            //Remove(2,2)       -> A partir do index 2 ele remove 2 caracteres
+            //Replace("a", "Z") -> Substitua a letra "a" por "Z"                        
+
+            
+            // DateTime
+            //Imprime uma lista de produtos que tem o DataVencimento > 3
+            var nomes = produtos.Where(p => p.DataVencimento.Day > 3)
+                              .Select(p => new ProdutoSelecionado { Name = p.Nome, Value = p.Valor, DiaDeVencimento = p.DataVencimento.Day })
+                              .ToList();
+
+            nomes.ForEach(item =>
+            {
+                Console.WriteLine(JsonConvert.SerializeObject(item));
+            });
+            Console.ReadKey();
+
+            */
+
+
+
+
+
+
+            //  -----------------------------------------------------  //
+            //Pesquisa - Aula 05
+
+            var produtos = new Produto().Listar();
+
+            
+            //Retorna o primeiro item da lista e se a lista estiver vazia ele preenche com NULL
+            var produto1 = produtos.FirstOrDefault();
+
+            //Retorna o primeiro item da lista e se a lista estiver vazia da Exception 
+            var produto2 = produtos.First();
+
+            //Retorna o ultimo item da lista e se a lista estiver vazia ele preenche com NULL
+            var produto3 = produtos.LastOrDefault();
+
+            //Retorna o ultimo item da lista e se a lista estiver vazia da Exception 
+            var produto4 = produtos.Last();
+
+            //Verifica se tem alguma coisa na lista
+            if(produtos.Any())
+            {
+                Console.WriteLine("Tem itens");
+            }
+
+                        
+            var produtos2 = new List<Produto>();
+            produtos2.Add(new Produto() { Nome = "Galaxy", Valor = 2500 });
+            produtos2.Add(new Produto() { Nome = "IPhone", Valor = 10000 });
+
+            //Adiciona uma lista ao final de outra lista
+            produtos.AddRange(produtos2);
+            produtos.ForEach(x => Console.WriteLine(x.Nome));
+
+            
+            int[] numerosPares = { 2, 4, 6, 8, 10 };
+            int[] numerosImpares = { 1, 3, 5, 7, 9 };
+            int[] numerosMistos = { 1, 2, 3, 4 };
+
+            //Verifica se tem numeros iguais nas duas listas (interseção)
+            var numeroQueSeRepetem = numerosMistos.Intersect(numerosPares);
+
+            //Verifica se tem numeros diferentes nas duas listas (exceção)
+            var numerosQueNaoSeRepetem = numerosMistos.Except(numerosPares);
+
+            //Cria um Enumerable com 10, 11 e 12
+            var sequencia = Enumerable.Range(10, 3);
+
+            //Repete a string "Gui" 10 vezes
+            var repetir = Enumerable.Repeat("Gui", 10);
+
+            Console.ReadKey();
+        }
+    }
+
+    public class ProdutoSelecionado
+    {
+        public string Name { get; set; }
+        public decimal Value { get; set; }
+        public int DiaDeVencimento { get; set; }
+
+        /*
+        public void ImprimeProduto(string name, decimal value)
+        {
+            Name = name;
+            Value = value;
+            
+            Console.WriteLine(Name);
+            Console.WriteLine(Value);
+        }*/
+    }
+}
---
+++ LojaDominio/Entidade/Produto.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LojaDominio.Entidade
+{
+    public class Produto
+    {
+
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public int Quantidade { get; set; }
+        public decimal Valor { get; set; }
+        public DateTime DataVencimento { get; set; }
+
+        public Produto()
+        {
+            _produtos = new List<Produto>();
+        }
+
+
+        private List<Produto> _produtos;       
+
+        public List<Produto> Listar()
+        {
+            _produtos.Add(new Produto() { Id = Guid.NewGuid(), Nome = "Banana", Quantidade = 5, Valor = 5, DataVencimento = DateTime.Now.AddDays(5) });
+            _produtos.Add(new Produto() { Id = Guid.NewGuid(), Nome = "Morango", Quantidade = 2, Valor = 10, DataVencimento = DateTime.Now.AddDays(2) });
+            _produtos.Add(new Produto() { Id = Guid.NewGuid(), Nome = "Pera", Quantidade = 5, Valor = 15, DataVencimento = DateTime.Now.AddDays(10) });
+            _produtos.Add(new Produto() { Id = Guid.NewGuid(), Nome = "Abacaxi", Quantidade = 1, Valor = 8, DataVencimento = DateTime.Now.AddDays(6) });
+            _produtos.Add(new Produto() { Id = Guid.NewGuid(), Nome = "Melão", Quantidade = 35, Valor = 13, DataVencimento = DateTime.Now.AddDays(13) });
+
+            return _produtos;
+        }
+
+
+
+
+        
+
+
+    }
+}
